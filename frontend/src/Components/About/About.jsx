bf49a67dda4f957064d0b1b8663e1162cb551a20
sarara497 <sara19972020@outlook.com>
2021-01-16 14:16:40 +0200

    fixed about bugs

--- frontend/src/Components/About/About.jsx
+++ frontend/src/Components/About/About.jsx
@@ -12,19 +12,23 @@ const About = () => {
             <h1  style={{ marginBottom: '60px' }}> About Me </h1>
         </Slide>
 
-        <Slide left>
+        
             <div className='about__cointainer'>
+            <Slide left>
                 <div id="border" >
                 <img id='about__pic'  src={profilepic} alt="profile"/>
                 </div >
+            </Slide>
+            <Slide right>
                 <div id='about__content' >
                     <h4 className='about__disc'> A graduate of Applied Computing, I joined in this specialty with desire and love in computers , and I am a software developer with robust problem-solving skills and proven experience in creating and designing software in a test-
                                driven environment, I am looking for a job to increase my experience and employ my scientific knowledge in the job market. </h4>
                     <a href='https://drive.google.com/file/d/1VWk4y6XfKwO3Oltj_yVMmZXJdwTcpZB-/view?usp=sharing' target="_blank" className='btn'>My Resume</a>
                    
                 </div>
+                </Slide>
             </div>
-            </Slide>
+           
 
 
         </div>
