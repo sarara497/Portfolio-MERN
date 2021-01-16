857dab183ff16131a8bb871d38a07148e2fe9835
sarara497 <sara19972020@outlook.com>
2021-01-16 19:27:20 +0200

    fixed bugs the About  section

--- frontend/src/Components/About/About.jsx
+++ frontend/src/Components/About/About.jsx
@@ -1,39 +1,44 @@
-import React from 'react'
-import Slide from 'react-reveal/Slide';
-import './about.css'
+import React from "react";
+import Slide from "react-reveal/Slide";
+import "./about.css";
 
-import profilepic from '../../Photos/Profilephoto.jpg'
+import profilepic from "../../Photos/Profilephoto.jpg";
 
 const About = () => {
-    return (
-        
-        <div className='about'>
+  return (
+    <div className="about">
+      <Slide left>
+        <h1 style={{ marginBottom: "60px" }}> About Me </h1>
+      </Slide>
+
+      <div className="about__cointainer">
         <Slide left>
-            <h1  style={{ marginBottom: '60px' }}> About Me </h1>
+          <div id="border">
+            <img id="about__pic" src={profilepic} alt="profile" />
+          </div>
         </Slide>
+        <Slide right>
+          <div id="about__content">
+            <h4 className="about__disc">
+              A graduate of Applied Computing, I joined in this specialty with
+              desire and love in computers , and Full-Stack developer who is
+              passionate about programming and figuring out everything related
+              to new technologies and very good at critical-thinking and problem
+              solving, I am looking for a job to increase my experience and
+              employ my scientific knowledge in the job market.
+            </h4>
+            <a
+              href="https://drive.google.com/file/d/1VWk4y6XfKwO3Oltj_yVMmZXJdwTcpZB-/view?usp=sharing"
+              target="_blank"
+              className="btn"
+            >
+              My Resume
+            </a>
+          </div>
+        </Slide>
+      </div>
+    </div>
+  );
+};
 
-        
-            <div className='about__cointainer'>
-            <Slide left>
-                <div id="border" >
-                <img id='about__pic'  src={profilepic} alt="profile"/>
-                </div >
-            </Slide>
-            <Slide right>
-                <div id='about__content' >
-                    <h4 className='about__disc'> A graduate of Applied Computing, I joined in this specialty with desire and love in computers , and I am a software developer with robust problem-solving skills and proven experience in creating and designing software in a test-
-                               driven environment, I am looking for a job to increase my experience and employ my scientific knowledge in the job market. </h4>
-                    <a href='https://drive.google.com/file/d/1VWk4y6XfKwO3Oltj_yVMmZXJdwTcpZB-/view?usp=sharing' target="_blank" className='btn'>My Resume</a>
-                   
-                </div>
-                </Slide>
-            </div>
-           
-
-
-        </div>
-        
-    )
-}
-
-export default About
+export default About;
