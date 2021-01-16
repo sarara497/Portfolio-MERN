43066aef56759f6a5b91397447c6cabf120235ac
sarara497 <sara19972020@outlook.com>
2021-01-16 21:16:07 +0200

    Fix Bugs

--- frontend/src/Components/ContactME/ContactMe.jsx
+++ frontend/src/Components/ContactME/ContactMe.jsx
@@ -25,13 +25,8 @@ const ContactMe = () => {
       <div className="forLinks">
   
       <a  href="https://github.com/sarara497" target="_blank"><GitHubIcon/> </a> 
-      
-  
       <a  href="https://www.linkedin.com/in/sara-alayed4197/" target="_blank"> <LinkedInIcon/> </a>
-      
-  
       <a  href="https://www.facebook.com/sara.ayed.9237" target="_blank"> <FacebookIcon/> </a> 
-    
       <a className="sidebar__item sidebar__email" onClick={handleEmailMe}> <EmailIcon/> </a>
       
       </div>
