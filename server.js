8836fe127461ab1fa31084ba1e0cc029484374e5
sarara497 <sara19972020@outlook.com>
2021-01-17 13:34:53 +0200

    Deploy

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
 
-app.use('/Projects' , require('./routes/Projects'))
+app.use('/projects' , require('./routes/projects'))
 
 
 if(process.env.NODE_ENV === 'production'){ 
