cf7f4385882731758d578fe355fd9e55dae6a903
sarara497 <sara19972020@outlook.com>
2021-03-11 19:41:53 +0200

    edit cv

--- frontend/src/Components/About/About.jsx
+++ frontend/src/Components/About/About.jsx
@@ -28,7 +28,7 @@ const About = () => {
               employ my scientific knowledge in the job market.
             </h4>
             <a
-              href="https://drive.google.com/file/d/1qubA5XSu9HOyXOEgaOus5CbDct1I2jOU/view?usp=sharing"
+              href="https://drive.google.com/file/d/1e1HexwcMOIJrpkLjrOuzVLuxDwPhUnnk/view?usp=sharing"
               target="blank"
               className="btnn"
             >
